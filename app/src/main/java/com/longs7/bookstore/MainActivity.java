84d01fa817e0cb9dc09cec160dd3a806b4269f0b
longs7 <longkeyvic1@gmail.com>
2021-05-26 08:10:09 +0700

    first commit

---
+++ app/src/main/java/com/longs7/bookstore/MainActivity.java
@@ -0,0 +1,14 @@
+package com.longs7.bookstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
